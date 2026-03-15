9a35a99b5046874e4ff74eac158bba49269086bf
KavyaJP <kavya31052005@gmail.com>
2026-03-15 14:21:22 +0530

    Added a basic sprite and script to test

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Ready Function, runs only once");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
